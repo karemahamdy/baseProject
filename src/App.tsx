161cd612910b0c70f5c4f72114281eb3a990e29b
karemahamdy <karemahamdy51@gmail.com>
2025-07-07 16:47:48 +0300

    chore: install all dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import { Button } from "@/components/ui/button"
+
 
 function App() {
   return (
     <>
-    
-      <p className="read-the-docs">
+      <div className="flex min-h-svh flex-col items-center justify-centerp-4 ">
+      <Button className='bg-red-500'>Click me</Button>
+    </div>
+      <p className="bg-blue-500 text-white p-4 rounded-lg">
       Hello karema!
       </p>
     </>
